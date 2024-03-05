ee951357fd42c342a4f2a41568a0b21acf679353
Kafrita-97 <despierta-tiempo-0c@icloud.com>
2024-03-05 03:44:02 +0100

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Juanma on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
